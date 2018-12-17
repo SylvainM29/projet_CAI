725d0a80f51b860370b52274c6b6abde50415cd3
Sylvain MILBEO <37792045+SylvainM29@users.noreply.github.com>
2018-12-18 00:40:19 +0100

    Formulaire qui marche mais qui communique pas

--- angular_front/src/app/form-proposition/form-proposition.component.ts
+++ angular_front/src/app/form-proposition/form-proposition.component.ts
@@ -1,5 +1,7 @@
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormPropositionService } from '../services/form-proposition.service';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-form-proposition',
@@ -9,13 +11,21 @@ import { FormPropositionService } from '../services/form-proposition.service';
 export class FormPropositionComponent implements OnInit {
 
   date = new Date();
-  constructor(private formPropositionService: FormPropositionService) {
-  }
+  constructor(private formPropositionService: FormPropositionService,
+              private router: Router) {  }
 
   ngOnInit() {}
 
   onSubmit(form: NgForm){
     console.log(form.value);
+    const auteur = form.value['auteur'];
+    const name = form.value['name'];
+    const brasserie = form.value['brasserie'];
+    const typeB = form.value['type'];
+    const degre = form.value['degre'];
+    const region = form.value['region'];
+    this.formPropositionService.newProposition(auteur, name, brasserie, typeB, degre, region);
+    this.router.navigate(['/catalogue']);
   }
 
 }
--- angular_front/src/app/services/form-proposition.service.ts
+++ angular_front/src/app/services/form-proposition.service.ts
@@ -1,14 +1,13 @@
 export class FormPropositionService{
 
-  newProposition(auteur: string, name: string, brasserie: string, type: string, degre: string, region: string, date: string){
+  newProposition(auteur: string, name: string, brasserie: string, type: string, degre: string, region: string){
     const formProposition = {
       auteur :'',
       name :'',
       brasserie:'',
       typeB:'',
       degre:'',
-      region:'',
-      date:''
+      region:''
     };
     formProposition.auteur = auteur;
     formProposition.name = name;
@@ -16,6 +15,6 @@ export class FormPropositionService{
     formProposition.typeB = type;
     formProposition.degre = degre;
     formProposition.region = region;
-    formProposition.date = date;
+    console.log(formProposition);
   }
 }
